c0d4e63780f26ea1b872d67462b2e57d980e4f15
xander cage <xcage01@hotmail.com>
2016-07-23 19:48:47 +0530

    custom class and timeout added

--- sleek-notify.js
+++ sleek-notify.js
@@ -2,22 +2,51 @@
 function sleekNotify (timeout) {
 	this.timeout = timeout;
 	this.className = "";
+	this.parent = document.createElement("div");
+	this.parent.style.width = "300px";
+	this.parent.style.position = "fixed";
+	this.parent.style.right = "40px";
+	this.parent.style.top = "30px";
+	document.body.appendChild(this.parent);
+
 	this.alert = function(message) {
 		var div = document.createElement("div");
 		div.style.width = "300px";
-		div.style.position = "fixed";
-		div.style.right = "40px";
-		div.style.top = "40px";
 		div.style.padding = "10px";
+		div.style.marginTop = "8px";
 		div.style.borderRadius = "3px";
 		div.style.border = "1px solid black";
-		// div.style.color = "white";
 		div.className += " "+this.className;
 		div.innerHTML = message;
-		document.body.appendChild(div);
+		this.parent.appendChild(div);
 		var currentInstance = this;
-		console.log("Timeout : "+this.timeout);
 		setTimeout(function(){div.remove();},this.timeout*1000);
+	};
+	this.alertTime = function(message,timeout) {
+		var div = document.createElement("div");
+		div.style.width = "300px";
+		div.style.padding = "10px";
+		div.style.marginTop = "8px";
+		div.style.borderRadius = "3px";
+		div.style.border = "1px solid black";
+		div.className += " "+this.className;
+		div.innerHTML = message;
+		this.parent.appendChild(div);
+		var currentInstance = this;
+		setTimeout(function(){div.remove();},timeout*1000);
+	};
+	this.alertClass = function(message,className) {
+		var div = document.createElement("div");
+		div.style.width = "300px";
+		div.style.padding = "10px";
+		div.style.marginTop = "8px";
+		div.style.borderRadius = "3px";
+		div.style.border = "1px solid black";
+		div.className += " "+this.className+" "+className;
+		div.innerHTML = message;
+		this.parent.appendChild(div);
+		var currentInstance = this;
+		setTimeout(function(){div.remove();},timeout*1000);
 	};
 	this.addClass = function(className) {
 		this.className = className;
